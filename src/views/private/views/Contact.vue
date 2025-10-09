2246ed984267509e8b94a0a1e31596ef4b1acefd
Joaquín Reyes <joaq.reyesm@gmail.com>
2025-10-09 11:53:46 -0300

    decimo commit

--- src/views/private/views/Contact.vue
+++ src/views/private/views/Contact.vue
@@ -444,10 +444,10 @@
                     </tbody>
                 </table>
             </form>
-            <button type="submit" v-if="!this.$route.params.companyId" @click="changeContact()"
+            <!-- <button type="submit" v-if="!this.$route.params.companyId" @click="changeContact()"
                 class="mt-5 px-3 text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm py-3.5 mb-6 text-center">
                 Cambiar de contacto
-            </button>
+            </button> -->
         </div>
     </div>
 </template>
